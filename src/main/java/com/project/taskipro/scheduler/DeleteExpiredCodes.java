f438fd9f7f2f3b9c0e0a4e46af3f538c965985d8
ivakov <ivakov.andrey.2004@mail.ru>
2025-04-02 23:20:57 +0300

    fixed cron expression

--- src/main/java/com/project/taskipro/scheduler/DeleteExpiredCodes.java
+++ src/main/java/com/project/taskipro/scheduler/DeleteExpiredCodes.java
@@ -3,6 +3,7 @@ package com.project.taskipro.scheduler;
 import com.project.taskipro.model.codes.Code;
 import com.project.taskipro.repository.CodesRepository;
 
+import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,19 +14,12 @@ import java.util.List;
 
 @Component
 @EnableScheduling
+@AllArgsConstructor
 public class DeleteExpiredCodes {
-
-//    @Value("${app.schedule.cron}")
-//    private final String cronValue;
 
     private final CodesRepository codesRepository;
 
-    public DeleteExpiredCodes(CodesRepository codesRepository) {
-        //this.cronValue = cronValue;
-        this.codesRepository = codesRepository;
-    }
-
-    @Scheduled(cron = "0 */30 * * * *")
+    @Scheduled(cron = "${app.schedule.cron}")
     public void deleteExpiredCodes() {
         List<Code> listCodeCodes = codesRepository.getAllByCodeExpireTimeBefore(LocalDateTime.now().plusMinutes(5));
         if(!listCodeCodes.isEmpty()){
